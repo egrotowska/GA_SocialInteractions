a6255b5774b8b91b289d4b1c27e4c0b3c6cb54d8
V. <wmswiderska@gmail.com>
2012-11-24 12:00:13 +0100

    Added game interface

---
+++ GA_SocialInteractions/Game.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GA_SocialInteractions
+{
+    interface Game
+    {
+        double cooperatorCooperatorPayoff;
+        double cooperatorDefectorPayoff;
+        double defectorCooperatorPayoff;
+        double defectorDefectorPayoff;
+
+        public double cooperatorCooperatorPayoff
+        {
+            get;
+        }
+        public double cooperatorDefectorPayoff
+        {
+            get;
+        }
+        public double defectorCooperatorPayoff
+        {
+            get;
+        }
+        public double defectorDefectorPayoff
+        {
+            get;
+        }
+
+        public void generatePayoffs();
+    }
+}
