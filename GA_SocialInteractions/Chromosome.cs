0745c7794ca529cb7e55cd13135943d79ece18e8
Ewelina Grotowska <ewelina.grotowska@gmail.com>
2012-11-27 13:21:27 +0100

    static Knapsack field -> Chromosome changes

--- GA_SocialInteractions/Chromosome.cs
+++ GA_SocialInteractions/Chromosome.cs
@@ -6,50 +6,38 @@ using System.Text;
 namespace GA_SocialInteractions {
     class Chromosome {
 
-        bool[] gens;
+        bool[] genes;
         bool ifCheater;
 
         public int Count
         {
-            get { return gens.Length; }
+            get { return genes.Length; }
         }
 
-        // random chromosome
         public Chromosome(int size)
         {
-            this.gens = new bool[size];
-
-            for (int j = 0; j < size; j++)
-            {
-                this.gens[j] = Convert.ToBoolean(GA_GT.random.Next() % 2);
-            }
-        }
-
-        // feasible chromosome
-        public Chromosome(int size, Knapsack knapsack)
-        {
-            this.gens = new bool[size];
+            this.genes = new bool[size];
             do
             {
                 for (int j = 0; j < size; j++)
                 {
-                    this.gens[j] = Convert.ToBoolean(GA_GT.random.Next() % 2);
+                    this.genes[j] = Convert.ToBoolean(GA_GT.random.Next() % 2);
                 }
-            } while (!IsFeasible(knapsack));
+            } while (!IsFeasible());
         }
 
-        public bool IsFeasible(Knapsack knapsack)
+        public bool IsFeasible()
         {
-            for (int m = 0; m < knapsack.NumberOfKnapsacks; m++)
+            for (int m = 0; m < GA_GT.knapsack.NumberOfKnapsacks; m++)
             {
                 int sum = 0;
 
-                for (int i = 0; i < gens.Length; i++)
+                for (int i = 0; i < genes.Length; i++)
                 {
-                    sum += gens[i] ? knapsack.GetWeight(m, i) : 0;
+                    sum += genes[i] ? GA_GT.knapsack.GetWeight(m, i) : 0;
                 }
 
-                if (sum > knapsack.GetConstraint(m))
+                if (sum > GA_GT.knapsack.GetConstraint(m))
                 {
                     return false;
                 }
@@ -58,26 +46,14 @@ namespace GA_SocialInteractions {
             return true;
         }
 
-        public bool getGen(int i)
-        {
-            return gens[i];
-        }
-
-        // instead of getGen(int i) we could use the indexer:
-        /*
         public bool this[int i]
         {
-	        get { return gens[i]; }
+	        get { return genes[i]; }
         }
-        */
-        // and then:
-        // Chromosome c = new ...
-        // bool b = c[5];
-        // I don't know if it's pretty so I leave the choice to you ;)
-
+        
         public void MutateGene(int i)
         {
-            gens[i] = !gens[i];
+            genes[i] = !genes[i];
         }
     }
 }
