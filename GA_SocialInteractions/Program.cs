12cd0d298a566274148384e51e4341d950b597a3
Ewelina Grotowska <ewelina.grotowska@gmail.com>
2012-12-03 21:17:52 +0100

    Added some real problem and parameters
    P01 from
    http://people.sc.fsu.edu/~jburkardt/datasets/knapsack_01/knapsack_01.html

--- GA_SocialInteractions/Program.cs
+++ GA_SocialInteractions/Program.cs
@@ -9,44 +9,54 @@ namespace GA_SocialInteractions
     {
         static void Main(string[] args)
         {
+            // solution: 1 1 1 1 0 1 0 0 0 0 0
+
             int numberOfObjects = 10;
             int numberOfKnapsacks = 1;
 
             int[][] weights = new int[numberOfKnapsacks][];
             int[] values = new int[numberOfObjects];
             int[] constraints = new int[numberOfKnapsacks];
-            
-            for (int i = 0; i < numberOfKnapsacks; i++)
-            {
-                weights[i] = new int[numberOfObjects];
 
-                for (int j = 0; j < numberOfObjects; j++)
-                {
-                    weights[i][j] = j + 1;
-                }
+            weights[0] = new int[numberOfObjects];
+            weights[0][0] = 23;
+            weights[0][1] = 31;
+            weights[0][2] = 29;
+            weights[0][3] = 44;
+            weights[0][4] = 53;
+            weights[0][5] = 38;
+            weights[0][6] = 63;
+            weights[0][7] = 85;
+            weights[0][8] = 89;
+            weights[0][9] = 82;
 
-                constraints[i] = 15;
-            }
+            values[0] = 92;
+            values[1] = 57;
+            values[2] = 49;
+            values[3] = 68;
+            values[4] = 60;
+            values[5] = 43;
+            values[6] = 67;
+            values[7] = 54;
+            values[8] = 87;
+            values[9] = 72;
 
-            for (int i = 0; i < numberOfObjects; i++)
-            {
-                values[i] = 2 * i;
-            }
-            
+            constraints[0] = 165;
+                        
             Knapsack knapsack = new Knapsack(weights, values, constraints);
             GA_GT ga_gt = new GA_GT();
 
             GA_GT.numberOfEpochs = 100;
-            GA_GT.chromosomeLength = 10;
+            GA_GT.chromosomeLength = numberOfObjects;
             GA_GT.populationSize = 10;
             GA_GT.gameModel = new PrisonersDilemma();
             GA_GT.knapsack = knapsack;
-            GA_GT.weightGA = 0.1;
-            GA_GT.weightGT = 0.1;
-            GA_GT.cheatingDegree = 0.1;
+            GA_GT.weightGA = 0.8;
+            GA_GT.weightGT = 0.2;
+            GA_GT.cheatingDegree = 50;
             GA_GT.cheaterRate = 0.1;
-            GA_GT.crossoverRate = 0.1;
-            GA_GT.mutationRate = 0.1;
+            GA_GT.crossoverRate = 0.75;
+            GA_GT.mutationRate = 1 / GA_GT.chromosomeLength;
 
             ga_gt.RunGA_GT();
             Console.Read();
